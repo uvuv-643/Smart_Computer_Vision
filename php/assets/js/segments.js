a4800fefdc26308c99ad5de88bae4ffbfec5b84f
artem <artem_zinatulin643@mail.ru>
2023-04-26 19:03:17 +0300

    Changed video module

---
+++ php/assets/js/segments.js
@@ -0,0 +1,70 @@
+(async () => {
+    const videoElement = document.querySelector('video');
+    const vidClips = [
+        'https://upload.wikimedia.org/wikipedia/commons/transcoded/f/f5/STB_Stuttgart_F%C3%B6hrich_U6_Line_Entering_Station_VIDEO.webm/STB_Stuttgart_F%C3%B6hrich_U6_Line_Entering_Station_VIDEO.webm.160p.webm',
+        'https://upload.wikimedia.org/wikipedia/commons/transcoded/8/87/Schlossbergbahn.webm/Schlossbergbahn.webm.160p.webm',
+        'https://mdn.github.io/learning-area/html/multimedia-and-embedding/video-and-audio-content/rabbit320.webm'
+    ];
+    // Shuffle clips. If you reload the page, you will get a random order of videos!
+    shuffleArr(vidClips);
+
+    // For `sourceBuffer.mode = 'segments'`, we have to be careful with multiple
+    // videos. Segments means that browser will try to sniff timestamps from
+    // the files. In our case, we are using completely separate files, without
+    // timeline info. So, we need to compute the duration of each, and then use
+    // that later to manually offset each chunk from the previous
+
+    /**
+     * @typedef {{url: string, duration: number, buff: ArrayBuffer}} ClipToAppend
+     */
+    /** @type {ClipToAppend[]} */
+    const clipsToAppend = await Promise.all(
+        vidClips.map(async (vidUrl) => {
+            const blob = await (await fetch(vidUrl)).blob();
+            const duration = await getDuration(blob);
+            const buff = await blob.arrayBuffer();
+            return {
+                url: vidUrl,
+                duration,
+                buff
+            };
+        })
+    );
+
+    // Normal setup, with MediaSource, Object URL, and prepped SourceBuffer
+    const mediaSource = new MediaSource();
+    videoElement.src = URL.createObjectURL(mediaSource);
+    // mode = segments
+    const sourceBuffer = await addSourceBufferWhenOpen(mediaSource, `video/webm; codecs="vp8,vorbis"`, 'segments');
+
+    /**
+     * Pointer to last vid appended out of source list
+     */
+    let clipIndex = 0;
+    sourceBuffer.onupdateend = () => {
+        if (clipIndex < clipsToAppend.length - 1) {
+            // We have another segment to add
+            // BUT, first we need to offset the time by the duration of
+            // the previously appended clip. Otherwise, it will overwrite it
+            sourceBuffer.timestampOffset += clipsToAppend[clipIndex].duration;
+            // Now we can move on to next clip and append it
+            clipIndex++;
+            sourceBuffer.appendBuffer(clipsToAppend[clipIndex].buff);
+        } else {
+            // Done!
+            mediaSource.endOfStream();
+            videoElement.play();
+        }
+    };
+
+    // This will kick off event listener chain above
+    sourceBuffer.appendBuffer(clipsToAppend[clipIndex].buff);
+
+    // Debug Info
+    console.log({
+        sourceBuffer,
+        mediaSource,
+        videoElement
+    });
+
+})();
--- php/video.php
+++ php/video.php
@@ -17,46 +17,7 @@
 
 </video>
 
-<script>
+<script type="module" src="assets/js/segments.js"></script>
 
-    var video = document.querySelector('video');
-    var assetURL = 'https://donntukhomichukpi20b.space/php/frag_bunny.mp4';
-    // assetURL = 'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4'
-    // assetURL = 'https://nickdesaulniers.github.io/netfix/demo/frag_bunny.mp4';
-
-    // Need to be specific for Blink regarding codecs
-    // ./mp4info frag_bunny.mp4 | grep Codec
-    var mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
-    var mediaSource = new MediaSource;
-    var sourceBuffer;
-
-    if ('MediaSource' in window && MediaSource.isTypeSupported(mimeCodec)) {
-        video.src = URL.createObjectURL(mediaSource);
-        mediaSource.addEventListener('sourceopen', function() {
-            sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
-            sourceOpen(assetURL)
-        });
-    } else {
-        console.error('Unsupported MIME type or codec: ', mimeCodec);
-    }
-
-    function sourceOpen (asset) {
-        fetchAB(asset, function (buf) {
-            sourceBuffer.appendBuffer(buf);
-        });
-    }
-
-    function fetchAB (url, cb) {
-        console.log(url);
-        var xhr = new XMLHttpRequest;
-        xhr.open('get', url);
-        xhr.responseType = 'arraybuffer';
-        xhr.onload = function () {
-            cb(xhr.response);
-        };
-        xhr.send();
-    }
-
-</script>
 </body>
 </html>
