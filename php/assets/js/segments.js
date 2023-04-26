a3130bb7ade29830723ca078cfcf0951c049e963
artem <artem_zinatulin643@mail.ru>
2023-04-26 19:29:56 +0300

    Changed video module

--- php/assets/js/segments.js
+++ php/assets/js/segments.js
@@ -9,7 +9,6 @@
         "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
         "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
         "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
-        "https://storage.googleapis.com/gtv-videos-bucket/sample/Sintel.jpg",
         "https://storage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4",
         "https://storage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
     ];
