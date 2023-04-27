802f93dcf6984a2e2727550b5f5e099381bf52d2
SmartOven <yarik.panteleev@gmail.com>
2023-04-27 22:32:08 +0300

    Updated design on login page

--- php/resources/views/layouts/guest.blade.php
+++ php/resources/views/layouts/guest.blade.php
@@ -11,6 +11,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <link rel="stylesheet" href="{{ asset('assets/css/global.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/css/login.css') }}">
 
         <!-- Scripts -->
