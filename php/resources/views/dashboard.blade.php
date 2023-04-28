c9407be378fac3956f474054e50631f028514c77
uvuv-643 <artem_zinatulin643@mail.ru>
2023-04-28 20:42:24 +0300

    Working will php server

--- php/resources/views/dashboard.blade.php
+++ php/resources/views/dashboard.blade.php
@@ -74,6 +74,7 @@
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" ></script>
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/js-cookie@3.0.0/dist/js.cookie.min.js"></script>
 
         <script>
 
