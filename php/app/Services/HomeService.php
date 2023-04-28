439be76d3b7a3cd90fc978fe45075ce8cba3001f
uvuv-643 <artem_zinatulin643@mail.ru>
2023-04-28 16:31:29 +0300

    Working will php server

--- php/app/Services/HomeService.php
+++ php/app/Services/HomeService.php
@@ -5,17 +5,58 @@ namespace App\Services;
 use App\Models\PeopleData;
 use Carbon\Carbon;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 
 class HomeService
 {
 
+    private function getGraphicData(string $timeInterval): Collection
+    {
+        if (preg_match('/^[a-z]/', $timeInterval, $matches)) {
+            $timeType = $matches[0];
+        }
+        $amountOfTime = intval($timeInterval);
+        if (!empty($timeType) && $amountOfTime > 0) {
+            switch ($timeType) {
+                case 'h': {
+                    return PeopleData::where('created_at', '>', Carbon::now()->subHours($amountOfTime))->get();
+                }
+                case 'd': {
+                    return PeopleData::where('created_at', '>', Carbon::now()->subDays($amountOfTime))->get();
+                }
+                default: {
+                    return PeopleData::where('created_at', '>', Carbon::now()->subMinutes($amountOfTime))->get();
+                }
+            }
+        }
+        return collect();
+    }
+
     public function getIndexPage(): View
     {
+        $isServiceAvailable = !!PeopleData::orderByDesc('created_at')
+          ->where('created_at', '>', Carbon::now()->subMinutes(1))
+          ->first();
+        if ($isServiceAvailable) {
+            $count = PeopleData::orderByDesc('created_at')
+              ->where('created_at', '>', Carbon::now()->subMinutes(1))
+              ->first()
+              ->count;
+        }
         $data = [
-          'available' => !!PeopleData::orderByDesc('created_at')->where('created_at', '>', Carbon::now()->subMinutes(5))->first()
+          'available' => $isServiceAvailable,
+          'count' => $count ?? null,
         ];
-        dd($data);
         return view('dashboard', $data);
+    }
+
+    public function getGraphic(Request $request): View
+    {
+        $graphicData = $this->getGraphicData('1d');
+        return view('components.graphic', [
+            'history' => $graphicData
+        ]);
     }
 
 }
--- php/resources/js/app.js
+++ php/resources/js/app.js
@@ -2,6 +2,9 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+import Chart from 'chart.js/auto'
+
+window.Chart = Chart
 window.Alpine = Alpine;
 
 Alpine.start();
