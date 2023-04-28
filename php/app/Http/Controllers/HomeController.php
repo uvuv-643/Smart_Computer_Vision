618cf85aa9ffc54fb1e750cbb8d061fc43e492b0
uvuv-643 <artem_zinatulin643@mail.ru>
2023-04-28 20:59:49 +0300

    Working will php server

--- php/app/Http/Controllers/HomeController.php
+++ php/app/Http/Controllers/HomeController.php
@@ -31,14 +31,14 @@ class HomeController extends Controller
         ]);
 
         PeopleData::query()->delete();
-        for ($i = 0; $i < 500; $i++) {
-            $rand = 50 + mt_rand(10, 50) * sin($i / 10 + mt_rand(0, 10) / 20);
-            PeopleData::insert([
-              'count' => $rand < 20 ? 0 : $rand,
-              'created_at' => Carbon::now()->subMinutes($i),
-              'updated_at' => Carbon::now()->subMinutes($i),
-            ]);
-        }
+//        for ($i = 0; $i < 500; $i++) {
+//            $rand = 50 + mt_rand(10, 50) * sin($i / 10 + mt_rand(0, 10) / 20);
+//            PeopleData::insert([
+//              'count' => $rand < 20 ? 0 : $rand,
+//              'created_at' => Carbon::now()->subMinutes($i),
+//              'updated_at' => Carbon::now()->subMinutes($i),
+//            ]);
+//        }
 
     }
 
