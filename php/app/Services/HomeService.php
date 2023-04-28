cc8e5657a977f9df1c44695a808167861379393e
uvuv-643 <artem_zinatulin643@mail.ru>
2023-04-28 15:54:46 +0300

    Working will php server

--- php/app/Services/HomeService.php
+++ php/app/Services/HomeService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use App\Models\PeopleData;
+use Carbon\Carbon;
 use Illuminate\Contracts\View\View;
 
 class HomeService
@@ -10,8 +12,9 @@ class HomeService
     public function getIndexPage(): View
     {
         $data = [
-          ''
+          'available' => !!PeopleData::orderByDesc()->where('timestamp', '>', Carbon::now()->subMinutes(5))->first()
         ];
+        dd($data);
         return view('dashboard', $data);
     }
 
