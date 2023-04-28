5e4f43715448c12f965ab2f35190fe40602af65a
uvuv-643 <artem_zinatulin643@mail.ru>
2023-04-28 20:17:26 +0300

    Working will php server

--- php/routes/api.php
+++ php/routes/api.php
@@ -20,3 +20,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('graphic-data', [HomeController::class, 'graphic'])->name('home.graphic.data');
+
+// define the addToken middleware
+Route::middleware('addToken')->get('/', function () {
+    return response()->json([
+      'message' => 'Hello World!'
+    ]);
+});
