9677b94e3e9078754884ba1c63514989b499633b
uvuv-643 <artem_zinatulin643@mail.ru>
2023-04-28 18:47:48 +0300

    Working will php server

--- php/app/Http/Controllers/HomeController.php
+++ php/app/Http/Controllers/HomeController.php
@@ -26,8 +26,9 @@ class HomeController extends Controller
     {
         PeopleData::query()->delete();
         for ($i = 0; $i < 500; $i++) {
+            $rand = 50 + mt_rand(10, 50) * sin($i / 10 + mt_rand(0, 10) / 20);
             PeopleData::insert([
-              'count' => 50 + mt_rand(10, 40) * sin($i / 10 + mt_rand(0, 10) / 20),
+              'count' => $rand < 10 ? 0 : $rand,
               'created_at' => Carbon::now()->subMinutes($i),
               'updated_at' => Carbon::now()->subMinutes($i),
             ]);
